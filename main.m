ead45380a87c7316d4ef4b5020f8b0039cb4651a
CristianChan <marxlenin2001@gmail.com>
2022-05-09 21:17:42 +0800

    commit change

--- main.m
+++ main.m
@@ -22,15 +22,18 @@ ylabel("Amp");
 %Interp
 f=interp(f,fs2/fs1);
 f=filter(Num,1,f);
-figure(7)
+figure(2)
 plot(t2,f);
+title('Wave of the interp signal');
+xlabel("n");
+ylabel("Amp");
 %%
 %I side and Q side Modulation
 I=f;%I sideroad
 Q=0;% Q sideroad
 %%
 c=cos(2*pi*fc*t2);%Carrier Signal
-figure(4)
+figure(3)
 plot(t2,c);
 title('Wave of the carrier signal');
 xlabel("n");
@@ -39,21 +42,67 @@ ylabel("Amp");
 %DSB Modulation
 %to be continued
 s=f.*c;
-figure(5)
+figure(4)
 plot(t2/0.001,s,t2/0.001,f,"r-");
 title('Wave of the moduled signal');
 xlabel('n');
 ylabel("Amp");
 legend('Modulated Message Signal','Message Signal m(n)')
 %%
-Tx=s+0;
+Tx=s+Q;
 %%
 %drawing the spectum of the signal
 N=length(Tx);
 If=abs(fftshift(fft(Tx,N)));%fft
 fi=fs2*(-N/2:N/2-1)/N;%digital freq=analog freq*T
-figure(6)
+figure(5)
 plot(fi,If);
-title('I路信号双边频域图像')
+title('发送信号双边频域图像')
 xlabel('rad');
 ylabel('|I(f)|');
+%%
+%Receiver part
+%%
+%Costas环
+fs3=15e6;  %采样频率
+ts3=1/fs3;
+num=15e7;%数据长度
+data=Tx;%科斯塔斯环的输入信号
+fc2=16e6;  %本地频率
+n=fs3/1000;  %累积时间为1ms
+nn=[0:n-1];
+nf=floor(length(data)/n);%将输入数据分成1ms的多个数据块
+wfc=2*pi*fc2;  %本地信号
+phi_prv=0;
+temp=0;
+frame=0;
+carrier_phase=0;
+phase=0;
+
+%环路滤波器的参数
+c1=153.7130;
+c2=6.1498;
+
+for frame=1:nf
+    %产生本地的sin和cos函数
+    expcol=exp(j*(wfc*ts3*nn+phase));
+    sine=imag(expcol);
+    cosine=real(expcol);
+    
+    x=data((1:n)+(frame-1)*n);
+    %将数据转换到基带
+    x_sine=x.*sine;
+    x_cosine=x.*cosine;
+    Q=sum(x_sine);
+    I=sum(x_cosine);
+    phase_discri(frame)=atan(Q/I);  %得到锁相环的输入
+    
+    %锁相环
+    dfrq=c1*phase_discri(frame)+temp; %经过环路滤波器
+    temp=temp+c2*phase_discri(frame); 
+    wfc=wfc-dfrq*2*pi;  %改变本地频率
+    dfrq_frame(frame)=wfc;
+    phase=wfc*ts3*n+phase; %得到不同块的频率
+    
+end
+mean_freq=mean(dfrq_frame/2/pi);
